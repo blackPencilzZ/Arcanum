b065343de1ebe048b8a31aebb5657d4c428d2d92
blackPencilzZ <zyp4job@163.com>
2017-09-12 15:35:16 +0800

    ArcanumPO

---
+++ arcanum-po/src/main/java/com/arcanumchina/arcanum/po/ArcanumPO.java
@@ -0,0 +1,30 @@
+package com.arcanumchina.arcanum.po;
+
+import java.time.Instant;
+
+public class ArcanumPO {
+
+	//基本属性
+	private Long aId;
+	private String aName;
+	
+	//拥有者
+	//private User owner;
+	
+	//分类属性
+	
+	private String descImgUrl;
+	private String descVideoUrl;
+	private String descText;
+	
+	//提交日期，发表日期
+	private Instant commitTime;
+	
+	//审核状态，通过，未通过
+	
+	//审核结果描述，是否合法，是否抄袭他人，存在问题，可以考虑
+	//用标签
+	
+	//分级
+	
+}
